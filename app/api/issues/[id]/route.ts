2071f313e8d3973446a617cbbf4c0dabdc966518
amgenious <henrytweneboah956@gmail.com>
2025-04-10 15:55:06 +0000

    done with update of error corrections

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -24,3 +24,35 @@ export async function DELETE(req: Request,  { params }: { params: Promise<{ id: 
     return NextResponse.json({ message: "Error deleting issue" }, { status: 500 })
   }
 }
+export async function PUT(req: Request,  { params }: { params: Promise<{ id: string }> }) {
+  try {
+    const body = await req.json()
+    const {transtype,transcode,customer,remarks } = body
+    if(!transtype || !transcode){
+      return NextResponse.json({message:"Missing issue parameter"},{status:400})
+    }
+    await connectToDatabase()
+    
+    const id = (await params).id;
+
+    if (!id) {
+      return NextResponse.json({ message: "Issue ID is required" }, { status: 400 })
+    }
+
+    const UpdateIssue = await Issues.findByIdAndUpdate(id,
+      { 
+       transtype:transtype,
+       transcode:transcode,
+       customer:customer,
+       remarks:remarks
+    },
+      { new: true })
+    if (!UpdateIssue) {
+      return NextResponse.json({ message: "Issue update not successful" }, { status: 404 })
+    }
+    return NextResponse.json({ message: "Issue updated successfully" }, { status: 200 })
+  } catch (error) {
+    console.error("Error updating issue:", error)
+    return NextResponse.json({ message: "Error updating issue" }, { status: 500 })
+  }
+}
--- app/api/receipt/[id]/route.ts
+++ app/api/receipt/[id]/route.ts
@@ -24,3 +24,37 @@ export async function DELETE(req: Request,  { params }: { params: Promise<{ id: 
     return NextResponse.json({ message: `Error deleting Receipt, ${error}` }, { status: 500 })
   }
 }
+
+export async function PUT(req: Request,  { params }: { params: Promise<{ id: string }> }) {
+  try {
+    const body = await req.json()
+    const {invoicenumber,transtype,transcode,supplier,remarks } = body
+    if(!transtype || !transcode){
+      return NextResponse.json({message:"Missing receipt parameter"},{status:400})
+    }
+    await connectToDatabase()
+    
+    const id = (await params).id;
+
+    if (!id) {
+      return NextResponse.json({ message: "Receipt ID is required" }, { status: 400 })
+    }
+
+    const UpdateIssue = await Receipt.findByIdAndUpdate(id,
+      { 
+       invoicenumber:invoicenumber,
+       transtype:transtype,
+       transcode:transcode,
+       supplier:supplier,
+       remarks:remarks
+    },
+      { new: true })
+    if (!UpdateIssue) {
+      return NextResponse.json({ message: "Receipt update not successful" }, { status: 404 })
+    }
+    return NextResponse.json({ message: "Receipt updated successfully" }, { status: 200 })
+  } catch (error) {
+    console.error("Error updating receipt:", error)
+    return NextResponse.json({ message: "Error updating receipt" }, { status: 500 })
+  }
+}
